775d9aa17e3b111028101f3a45d76063df924615
hrpofficial736 <hrpofficial736@gmail.com>
2024-07-13 20:20:52 +0530

    fourth

--- client/src/pages/PatientDashboard.jsx
+++ client/src/pages/PatientDashboard.jsx
@@ -1,15 +1,18 @@
 import React, {useState, useEffect} from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import { NavBar } from './LandingPage';
-
+import '../styles.css'
 
 const PatientDashboard = () => {
   return (
+    <>
+    <NavBar />
     <div id='dashboard-section'>
       <div id="personal-info">
 
       </div>
     </div>
+    </>
   )
 }
 
